9977ddc33682f29d0d43abd90214bed32d9daa14
Miguel Martinez <rodilla@gmail.com>
2023-06-04 20:26:17 +0200

    imageInTable example does not need the custom class anymore

--- examples/imageInTable.php
+++ examples/imageInTable.php
@@ -6,7 +6,7 @@ if (!file_exists($ext)) {
 }
 
 include $ext;
-$pdf = new CezTableImage('a4');
+$pdf = new Cezpdf('a4');
 
 $pdf->selectFont('Helvetica');
 
@@ -15,15 +15,17 @@ $image = '../ros.jpg';
 //$image = 'images/test_alpha.gif';
 
 $data = array(
-                ['num' => 1, 'name' => 'gandalf', 'type' => '<C:showimage:'.$image.' 90>'],
-                ['num' => 4, 'name' => 'saruman', 'type' => 'baddude', 'url' => 'http://sourceforge.net/projects/pdf-php'],
-                ['num' => 5, 'name' => 'sauron', 'type' => '<C:showimage:'.urlencode($image).' 90>'],
-                ['num' => 6, 'name' => 'sauron', 'type' => '<C:showimage:'.$image.'><C:showimage:'.$image.' 90>'."\nadadd"],
-                ['num' => 8, 'name' => 'sauron', 'type' => '<C:showimage:'.$image.' 90>'],
-                ['num' => 10, 'name' => 'sauron', 'type' => '<C:showimage:'.$image.' 50>'],
-                ['num' => 11, 'name' => 'sauron', 'type' => '<C:showimage:'.$image.'>'],
-                /* ['num'=>12,'name'=>'sauron','type'=>'<C:showimage:'.urlencode('http://myserver.mytld/myimage.jpeg'].'>'), */
-            );
+    ['num' => 1, 'name' => 'gandalf', 'type' => '<C:image:'.$image.',90>'],
+    ['num' => 4, 'name' => 'saruman', 'type' => 'baddude', 'url' => 'http://sourceforge.net/projects/pdf-php'],
+    //['num' => 5, 'name' => 'sauron', 'type' => '<C:image:'.urlencode($image).',90>'],
+    ['num' => 5, 'name' => 'sauron', 'type' => '<C:image:'.$image.',90>'],
+    ['num' => 6, 'name' => 'sauron', 'type' => '<C:image:'.$image.'><C:image:'.$image.',90>'."\nadadd"],
+    ['num' => 8, 'name' => 'sauron', 'type' => '<C:image:'.$image.',90>'],
+    ['num' => 10, 'name' => 'sauron', 'type' => '<C:image:'.$image.',50>'],
+    ['num' => 11, 'name' => 'sauron', 'type' => '<C:image:'.$image.'>'],
+    /* ['num'=>12,'name'=>'sauron','type'=>'<C:showimage:'.urlencode('http://myserver.mytld/myimage.jpeg'].'>'), */
+);
+
 
 $pdf->ezTable($data, '', '', ['width' => 400, 'showLines' => 2]);
 $pdf->ezText("\nWithout table width:");
